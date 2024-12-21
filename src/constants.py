d7bd0b6019c635b6c0a51ec70cbdf3914486478b
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-21 16:52:31 +0200

    change background color and setup grid and sell_size

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
 # WINDOW -----------------------------------------------------------------------
 WINDOW_SIZE = 600  # width and height of the window
+GRID_SIZE = 60  # number of cells in the grid (60x60)
+CELL_SIZE = WINDOW_SIZE // GRID_SIZE  # size of each cell
 
 # COLORS -----------------------------------------------------------------------
-BACKGROUND_COLOR = (255, 255, 255)
+BACKGROUND_COLOR = (255, 200, 150)
