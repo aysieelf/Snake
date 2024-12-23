25453097579f3a078e867ad999f6425bc6bfc826
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-23 21:20:37 +0200

    fix: add setUp to tests

--- tests/test_food_system.py
+++ tests/test_food_system.py
@@ -9,268 +9,234 @@ class FoodSystemShould(unittest.TestCase):
     def setUp(self):
         self.snake = Mock()
         self.particle_system = Mock()
+        self.food_system = FoodSystem(self.snake, self.particle_system)
 
     def test_init_initializesSuccessfully(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertIsNotNone(food_system)
+        self.assertIsNotNone(self.food_system)
 
     def test_init_setsFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertEqual((0, 0), food_system.food_pos)
+        self.assertEqual((0, 0), self.food_system.food_pos)
 
     def test_init_setsNewFood(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertTrue(food_system.new_food)
+        self.assertTrue(self.food_system.new_food)
 
     def test_init_setsBonusFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertIsNone(food_system.bonus_food_pos)
+        self.assertIsNone(self.food_system.bonus_food_pos)
 
     def test_init_setsBonusFoodActive(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertFalse(food_system.bonus_food_active)
+        self.assertFalse(self.food_system.bonus_food_active)
 
     def test_init_setsBonusFoodDurationTimer(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertEqual(0, food_system.bonus_food_duration_timer)
+        self.assertEqual(0, self.food_system.bonus_food_duration_timer)
 
     def test_init_setsBonusFoodSpawnTimer(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        self.assertEqual(0, food_system.bonus_food_spawn_timer)
+        self.assertEqual(0, self.food_system.bonus_food_spawn_timer)
 
     def test_updateBonusFood_returnsBonusFoodCollected_whenBonusFoodActive(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = True
+        self.food_system._bonus_food_active = True
 
-        with (patch.object(food_system, "_handle_bonus_food", return_value=True) as mock_handle_bonus_food):
+        with (patch.object(self.food_system, "_handle_bonus_food", return_value=True) as mock_handle_bonus_food):
 
-            result = food_system.update_bonus_food(None)
+            result = self.food_system.update_bonus_food(None)
             self.assertTrue(result)
             mock_handle_bonus_food.assert_called_once()
 
     def test_updateBonusFood_returnsFalse_whenBonusFoodNotActive(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = False
+        self.food_system._bonus_food_active = False
 
-        result = food_system.update_bonus_food(None)
+        result = self.food_system.update_bonus_food(None)
         self.assertFalse(result)
 
     def test_updateBonusFood_incrementsBonusFoodSpawnTimer_whenBonusFoodPosNone(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = None
+        self.food_system._bonus_food_pos = None
 
-        food_system.update_bonus_food(None)
-        self.assertEqual(1, food_system.bonus_food_spawn_timer)
+        self.food_system.update_bonus_food(None)
+        self.assertEqual(1, self.food_system.bonus_food_spawn_timer)
 
     def test_updateBonusFood_spawnsBonusFood_whenBonusFoodSpawnTimerEqualsBonusFoodSpawnInterval(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_spawn_timer = c.BONUS_FOOD_SPAWN_INTERVAL - 1
+        self.food_system._bonus_food_spawn_timer = c.BONUS_FOOD_SPAWN_INTERVAL - 1
 
-        with patch.object(food_system, "_spawn_bonus_food") as mock_spawn_bonus_food:
+        with patch.object(self.food_system, "_spawn_bonus_food") as mock_spawn_bonus_food:
 
-            food_system.update_bonus_food(None)
+            self.food_system.update_bonus_food(None)
             mock_spawn_bonus_food.assert_called_once()
 
     def test_updateBonusFood_deactivatesBonusFood_whenBonusFoodDurationTimerEqualsBonusFoodDuration(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = True
-        food_system._bonus_food_duration_timer = c.BONUS_FOOD_DURATION
+        self.food_system._bonus_food_active = True
+        self.food_system._bonus_food_duration_timer = c.BONUS_FOOD_DURATION
 
-        with (patch.object(food_system, "_handle_bonus_food", return_value=True) as mock_handle_bonus_food,
-                patch.object(food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
+        with (patch.object(self.food_system, "_handle_bonus_food", return_value=True) as mock_handle_bonus_food,
+                patch.object(self.food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
               ):
 
-            food_system.update_bonus_food(None)
-            self.assertTrue(food_system.bonus_food_active)
+            self.food_system.update_bonus_food(None)
+            self.assertTrue(self.food_system.bonus_food_active)
             mock_handle_bonus_food.assert_called_once()
             mock_deactivate_bonus_food.assert_called_once()
 
     def test_handleBonusFood_returnsTrue_whenSnakeHeadPositionEqualsBonusFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         self.snake.get_head_position.return_value = (10, 10)
 
-        with patch.object(food_system, "_collect_bonus_food") as mock_collect_bonus_food:
-            result = food_system._handle_bonus_food(None)
+        with patch.object(self.food_system, "_collect_bonus_food") as mock_collect_bonus_food:
+            result = self.food_system._handle_bonus_food(None)
             self.assertTrue(result)
             mock_collect_bonus_food.assert_called_once()
 
     def test_handleBonusFood_returnsFalse_whenSnakeHeadPositionNotEqualsBonusFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         self.snake.get_head_position.return_value = (10, 11)
 
-        result = food_system._handle_bonus_food(None)
+        result = self.food_system._handle_bonus_food(None)
         self.assertFalse(result)
 
     def test_handleBonusFood_incrementsBonusFoodDurationTimer_whenSnakeHeadPositionNotEqualsBonusFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         self.snake.get_head_position.return_value = (10, 11)
 
-        food_system._handle_bonus_food(None)
-        self.assertEqual(1, food_system.bonus_food_duration_timer)
+        self.food_system._handle_bonus_food(None)
+        self.assertEqual(1, self.food_system.bonus_food_duration_timer)
 
     def test_collectBonusFood_callsParticleSystemSpawnParticles(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         tail = Mock()
 
-        with (patch.object(food_system._particle_system, "spawn_particles") as mock_spawn_particles,
-              patch.object(food_system._snake, "grow") as mock_grow,
-              patch.object(food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
+        with (patch.object(self.food_system._particle_system, "spawn_particles") as mock_spawn_particles,
+              patch.object(self.food_system._snake, "grow") as mock_grow,
+              patch.object(self.food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
               ):
 
-            food_system._collect_bonus_food(tail)
+            self.food_system._collect_bonus_food(tail)
             mock_spawn_particles.assert_called_once()
 
     def test_collectBonusFood_callsSnakeGrow(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         tail = Mock()
 
-        with (patch.object(food_system._particle_system, "spawn_particles") as mock_spawn_particles,
-              patch.object(food_system._snake, "grow") as mock_grow,
-              patch.object(food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
+        with (patch.object(self.food_system._particle_system, "spawn_particles") as mock_spawn_particles,
+              patch.object(self.food_system._snake, "grow") as mock_grow,
+              patch.object(self.food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
               ):
 
-            food_system._collect_bonus_food(tail)
+            self.food_system._collect_bonus_food(tail)
             mock_grow.assert_called_once_with(tail)
 
     def test_collectBonusFood_deactivatesBonusFood(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
         tail = Mock()
 
-        with (patch.object(food_system._particle_system, "spawn_particles") as mock_spawn_particles,
-              patch.object(food_system._snake, "grow") as mock_grow,
-              patch.object(food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
+        with (patch.object(self.food_system._particle_system, "spawn_particles") as mock_spawn_particles,
+              patch.object(self.food_system._snake, "grow") as mock_grow,
+              patch.object(self.food_system, "deactivate_bonus_food") as mock_deactivate_bonus_food,
               ):
 
-            food_system._collect_bonus_food(tail)
+            self.food_system._collect_bonus_food(tail)
             mock_deactivate_bonus_food.assert_called_once()
 
     def test_spawnBonusFood_setsBonusFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system._spawn_bonus_food()
-            self.assertEqual((10, 10), food_system.bonus_food_pos)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system._spawn_bonus_food()
+            self.assertEqual((10, 10), self.food_system.bonus_food_pos)
 
     def test_spawnBonusFood_setsBonusFoodSpawnTimer(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_spawn_timer = 10
+        self.food_system._bonus_food_spawn_timer = 10
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system._spawn_bonus_food()
-            self.assertEqual(0, food_system.bonus_food_spawn_timer)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system._spawn_bonus_food()
+            self.assertEqual(0, self.food_system.bonus_food_spawn_timer)
 
     def test_spawnBonusFood_setsBonusFoodActive(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = False
+        self.food_system._bonus_food_active = False
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system._spawn_bonus_food()
-            self.assertTrue(food_system.bonus_food_active)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system._spawn_bonus_food()
+            self.assertTrue(self.food_system.bonus_food_active)
 
     def test_spawnBonusFood_setsBonusFoodDurationTimer(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_duration_timer = 10
+        self.food_system._bonus_food_duration_timer = 10
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system._spawn_bonus_food()
-            self.assertEqual(0, food_system.bonus_food_duration_timer)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system._spawn_bonus_food()
+            self.assertEqual(0, self.food_system.bonus_food_duration_timer)
 
     def test_spawnFood_setsFoodPos(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._new_food = True
+        self.food_system._new_food = True
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system.spawn_food()
-            self.assertEqual((10, 10), food_system.food_pos)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system.spawn_food()
+            self.assertEqual((10, 10), self.food_system.food_pos)
 
     def test_spawnFood_doesNotSetFoodPos_whenNewFoodFalse(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._new_food = False
+        self.food_system._new_food = False
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system.spawn_food()
-            self.assertEqual((0, 0), food_system.food_pos)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system.spawn_food()
+            self.assertEqual((0, 0), self.food_system.food_pos)
 
     def test_spawnFood_setsNewFoodFalse(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._new_food = True
+        self.food_system._new_food = True
 
-        with patch.object(food_system, "_validate_food_position", return_value=(10, 10)):
-            food_system.spawn_food()
-            self.assertFalse(food_system.new_food)
+        with patch.object(self.food_system, "_validate_food_position", return_value=(10, 10)):
+            self.food_system.spawn_food()
+            self.assertFalse(self.food_system.new_food)
 
     # TODO: Implement tests for _validate_food_position
     def test_validateFoodPosition_returnsValidPosition(self):
         pass
 
     def test_deactivateBonusFood_setsBonusFoodActiveToFalse(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = True
+        self.food_system._bonus_food_active = True
 
-        food_system.deactivate_bonus_food()
-        self.assertFalse(food_system.bonus_food_active)
+        self.food_system.deactivate_bonus_food()
+        self.assertFalse(self.food_system.bonus_food_active)
 
     def test_deactivateBonusFood_setsBonusFoodPosToNone(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
 
-        food_system.deactivate_bonus_food()
-        self.assertIsNone(food_system.bonus_food_pos)
+        self.food_system.deactivate_bonus_food()
+        self.assertIsNone(self.food_system.bonus_food_pos)
 
     def test_deactivateBonusFood_setsBonusFoodDurationTimerToZero(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_duration_timer = 10
+        self.food_system._bonus_food_duration_timer = 10
 
-        food_system.deactivate_bonus_food()
-        self.assertEqual(0, food_system.bonus_food_duration_timer)
+        self.food_system.deactivate_bonus_food()
+        self.assertEqual(0, self.food_system.bonus_food_duration_timer)
 
     def test_reset_setsFoodPosToZeros(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._food_pos = [10, 10]
+        self.food_system._food_pos = [10, 10]
 
-        food_system.reset()
-        self.assertEqual((0, 0), food_system.food_pos)
+        self.food_system.reset()
+        self.assertEqual((0, 0), self.food_system.food_pos)
 
     def test_reset_setsNewFoodToTrue(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._new_food = False
+        self.food_system._new_food = False
 
-        food_system.reset()
-        self.assertTrue(food_system.new_food)
+        self.food_system.reset()
+        self.assertTrue(self.food_system.new_food)
 
     def test_reset_setsBonusFoodPosToNone(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_pos = [10, 10]
+        self.food_system._bonus_food_pos = [10, 10]
 
-        food_system.reset()
-        self.assertIsNone(food_system.bonus_food_pos)
+        self.food_system.reset()
+        self.assertIsNone(self.food_system.bonus_food_pos)
 
     def test_reset_setsBonusFoodSpawnTimerToZero(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_spawn_timer = 10
+        self.food_system._bonus_food_spawn_timer = 10
 
-        food_system.reset()
-        self.assertEqual(0, food_system.bonus_food_spawn_timer)
+        self.food_system.reset()
+        self.assertEqual(0, self.food_system.bonus_food_spawn_timer)
 
     def test_reset_setsBonusFoodDurationTimerToZero(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_duration_timer = 10
+        self.food_system._bonus_food_duration_timer = 10
 
-        food_system.reset()
-        self.assertEqual(0, food_system.bonus_food_duration_timer)
+        self.food_system.reset()
+        self.assertEqual(0, self.food_system.bonus_food_duration_timer)
 
     def test_reset_setsBonusFoodActiveToFalse(self):
-        food_system = FoodSystem(self.snake, self.particle_system)
-        food_system._bonus_food_active = True
+        self.food_system._bonus_food_active = True
 
-        food_system.reset()
-        self.assertFalse(food_system.bonus_food_active)
+        self.food_system.reset()
+        self.assertFalse(self.food_system.bonus_food_active)
 
 
 
--- tests/test_snake.py
+++ tests/test_snake.py
@@ -6,113 +6,94 @@ from src.utils import constants as c
 
 
 class SnakeShould(unittest.TestCase):
+    def setUp(self):
+        self.snake = Snake()
 
     def test_init_initializesSuccessfully(self):
-        snake = Snake()
-        self.assertIsNotNone(snake)
+        self.assertIsNotNone(self.snake)
 
     def test_init_setsLengthToTwo(self):
-        snake = Snake()
-        self.assertEqual(2, snake.length)
+        self.assertEqual(2, self.snake.length)
 
     def test_init_setsPositions(self):
-        snake = Snake()
         self.assertEqual(
             ((10, 10), (11, 10)),
-            snake.positions,
+            self.snake.positions,
         )
 
     def test_init_setsDirection(self):
-        snake = Snake()
-        self.assertEqual((-1, 0), snake.direction)
+        self.assertEqual((-1, 0), self.snake.direction)
 
     def test_init_setsHasDirectionChangedToFalse(self):
-        snake = Snake()
-        self.assertFalse(snake.has_direction_changed)
+        self.assertFalse(self.snake.has_direction_changed)
 
     def test_getHeadPosition_returnsHeadPosition(self):
-        snake = Snake()
-        self.assertEqual((10, 10), snake.get_head_position())
+        self.assertEqual((10, 10), self.snake.get_head_position())
 
     def test_getSnakeBodyPositions_returnsBodyPositions(self):
-        snake = Snake()
         self.assertEqual(
             ((200, 200), (220, 200)),
-            snake.get_snake_body_positions(),
+            self.snake.get_snake_body_positions(),
         )
 
     def test_move_returnsTail(self):
-        snake = Snake()
-        with patch.object(snake, "_get_next_head_position", return_value=(9, 10)):
-            tail = snake.move()
+        with patch.object(self.snake, "_get_next_head_position", return_value=(9, 10)):
+            tail = self.snake.move()
             self.assertEqual((11, 10), tail)
 
     def test_move_updatesPositions(self):
-        snake = Snake()
-        with patch.object(snake, "_get_next_head_position", return_value=(9, 10)):
-            snake.move()
+        with patch.object(self.snake, "_get_next_head_position", return_value=(9, 10)):
+            self.snake.move()
             self.assertEqual(
                 ((9, 10), (10, 10)),
-                snake.positions,
+                self.snake.positions,
             )
 
     def test_move_setsHasDirectionChangedToFalse(self):
-        snake = Snake()
-        with patch.object(snake, "_get_next_head_position", return_value=(9, 10)):
-            snake.move()
-            self.assertFalse(snake.has_direction_changed)
+        with patch.object(self.snake, "_get_next_head_position", return_value=(9, 10)):
+            self.snake.move()
+            self.assertFalse(self.snake.has_direction_changed)
 
     def test_grow_addsTailToPositions(self):
-        snake = Snake()
-        snake.grow((8, 10))
+        self.snake.grow((8, 10))
         self.assertEqual(
             ((10, 10), (11, 10), (8, 10)),
-            snake.positions,
+            self.snake.positions,
         )
 
     def test_getNextHeadPosition_returnsNextHeadPosition(self):
-        snake = Snake()
-        self.assertEqual((9, 10), snake._get_next_head_position())
+        self.assertEqual((9, 10), self.snake._get_next_head_position())
 
     def test_changeDirection_validDirection_returnsTrue(self):
-        snake = Snake()
-        self.assertTrue(snake.change_direction(c.UP))
+        self.assertTrue(self.snake.change_direction(c.UP))
 
     def test_changeDirection_validDirection_updatesDirection(self):
-        snake = Snake()
-        snake.change_direction(c.UP)
-        self.assertEqual(c.UP, snake.direction)
+        self.snake.change_direction(c.UP)
+        self.assertEqual(c.UP, self.snake.direction)
 
     def test_changeDirection_validDirection_setsHasDirectionChangedTrue(self):
-        snake = Snake()
-        snake.change_direction(c.UP)
-        self.assertTrue(snake.has_direction_changed)
+        self.snake.change_direction(c.UP)
+        self.assertTrue(self.snake.has_direction_changed)
 
     def test_changeDirection_oppositeDirection_returnsFalse(self):
-        snake = Snake()  # LEFT
-        self.assertFalse(snake.change_direction(c.RIGHT))
+        self.assertFalse(self.snake.change_direction(c.RIGHT))
 
     def test_changeDirection_oppositeDirection_doesNotUpdateDirection(self):
-        snake = Snake()  # LEFT
-        snake.change_direction(c.RIGHT)
-        self.assertEqual(c.LEFT, snake.direction)
+        self.snake.change_direction(c.RIGHT)
+        self.assertEqual(c.LEFT, self.snake.direction)
 
     def test_changeDirection_invalidDirection_returnsFalse(self):
-        snake = Snake()
-        self.assertFalse(snake.change_direction((2, 2)))
+        self.assertFalse(self.snake.change_direction((2, 2)))
 
     def test_changeDirection_invalidDirection_doesNotUpdateDirection(self):
-        snake = Snake()
-        snake.change_direction((2, 2))
-        self.assertEqual(c.LEFT, snake.direction)
+        self.snake.change_direction((2, 2))
+        self.assertEqual(c.LEFT, self.snake.direction)
 
     def test_changeDirection_afterDirectionChanged_returnsFalse(self):
-        snake = Snake()
-        snake.change_direction(c.UP)
-        self.assertFalse(snake.change_direction(c.RIGHT))
+        self.snake.change_direction(c.UP)
+        self.assertFalse(self.snake.change_direction(c.RIGHT))
 
     def test_changeDirection_afterDirectionChanged_doesNotUpdateDirection(self):
-        snake = Snake()
-        snake.change_direction(c.UP)
-        snake.change_direction(c.RIGHT)
-        self.assertEqual(c.UP, snake.direction)
+        self.snake.change_direction(c.UP)
+        self.snake.change_direction(c.RIGHT)
+        self.assertEqual(c.UP, self.snake.direction)
