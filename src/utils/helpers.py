aed85f2eaef98ee2cb3ac285a08b2a774975768b
aysieelf <aysel.karadayi.a63@learn.telerikacademy.com>
2024-12-28 23:01:32 +0200

    update documentation for helpers.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -3,8 +3,11 @@ import random
 from src.utils import constants as c
 
 
-def get_random_position():
+def get_random_position() -> list[int]:
     """
     Get a random position on the grid
+
+    Returns:
+        list[int]: A list with two integers representing the x and y coordinates
     """
     return [random.randint(1, c.GRID_SIZE - 2), random.randint(1, c.GRID_SIZE - 2)]
